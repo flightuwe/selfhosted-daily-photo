7856a291bd632e65b38eda135077d2e1009768b6
flightuwe <dumper@posteo.de>
2026-03-08 19:06:23 +0100

    Bump Android version to 0.3.19

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.selfhosted.daily"
         minSdk = 26
         targetSdk = 34
-        versionCode = 20
-        versionName = "0.3.18"
+        versionCode = 21
+        versionName = "0.3.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "API_BASE_URL", "\"https://daily.teacloud.synology.me/api/\"")
