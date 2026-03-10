0d7c122d06367ece3722788bbb029cb1414f8578
flightuwe <dumper@posteo.de>
2026-03-10 21:27:48 +0100

    Release Android v0.3.50

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.selfhosted.daily"
         minSdk = 26
         targetSdk = 34
-        versionCode = 50
-        versionName = "0.3.49"
+        versionCode = 51
+        versionName = "0.3.50"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "API_BASE_URL", "\"https://daily.teacloud.synology.me/api/\"")
