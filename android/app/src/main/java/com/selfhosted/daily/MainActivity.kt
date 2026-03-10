a7d12b0cd0264b2d9e3ff41a661d1fd7879ee2a1
flightuwe <dumper@posteo.de>
2026-03-10 14:19:10 +0100

    Fix release compile error and bump Android 0.3.37

--- android/app/src/main/java/com/selfhosted/daily/MainActivity.kt
+++ android/app/src/main/java/com/selfhosted/daily/MainActivity.kt
@@ -35,6 +35,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
