253e1d1253d0ac662911dfe90d5dab77ce8294b4
flightuwe <dumper@posteo.de>
2026-03-12 18:32:41 +0100

    Prepare v0.4.7 release notes and Android version bump

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.selfhosted.daily"
         minSdk = 26
         targetSdk = 34
-        versionCode = 59
-        versionName = "0.4.6"
+        versionCode = 60
+        versionName = "0.4.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "API_BASE_URL", "\"https://daily.teacloud.synology.me/api/\"")
